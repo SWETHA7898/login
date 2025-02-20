1c1d8a9f6fc63aca5f8d674f3e4005810d0469b7
Swetha <swethamurugesh1@gmail.com>
2025-02-20 11:30:23 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
-const express = require("express");
-const app = express();
+const express=require("express")
+const cors=require("cors")
 
-const port = process.env.PORT || 8080;
+const app=express()
+app.use(cors())
+var uname="swetha"
+var pass=123
 
-app.get("/", (req, res) => {
-  res.send("Subscribe to Arpan Neupane's channel");
-});
+app.use(express.json())
 
-app.listen(port, () => {
-  `Server started on port ${port}`;
-});
+app.use(express.urlencoded({extended:true}))
+
+app.post("/login",(req,res)=>{
+    console.log(req.body)
+    if(uname===req.body.username && pass==req.body.password){
+        res.send(true)
+    }
+    else{
+        res.send(false)
+    }
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("Server started...")
+   
+    
+})
